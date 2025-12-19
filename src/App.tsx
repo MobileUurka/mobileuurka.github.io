e9b51988a0a8af1d84a3de872c4d24b148d17187
Darryl Mbae <darrylmbae01@gmail.com>
2025-12-19 13:33:23 +0300

    Remove CTA component from application
    - Remove CTA import and usage from App.tsx
    - Clean up unused component references
    - Maintain Request Demo functionality in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,27 @@ import About from './components/About'
 import Powered from './components/Powered'
 import Sabi from './components/Sabi'
 import FAQs from './components/FAQs'
-import CTA from './components/CTA'
+
 import Pricing from './components/Pricing'
 import Footer from './components/Footer'
 
 function App() {
-    const [ navClicked,setNavClicked ] = useState(false)
+  const [navClicked, setNavClicked] = useState(false)
 
 
   return (
     <>
-    <Navbar navClicked={navClicked} setNavClicked={setNavClicked}/>
-    <SideBar navClicked={navClicked} setNavClicked={setNavClicked} />
-    <Hero/>
-    <div id="metrics" className="pt-20"><Metrics/></div>
-    <div id="about" className="pt-20"><About/></div>
-    <div id="powered" className="pt-20"><Powered/></div>
-    <div id="sabi" className="pt-20"><Sabi/></div>
-    <Pricing/>
-    <div id="faqs" className="pt-20"><FAQs/></div>
-    {/* <div id="cta" className="pt-20"><CTA/></div> */}
-    <div id="footer" className="pt-20"><Footer/></div>
+      <Navbar navClicked={navClicked} setNavClicked={setNavClicked} />
+      <SideBar navClicked={navClicked} setNavClicked={setNavClicked} />
+      <Hero />
+      <div id="metrics" className="pt-20"><Metrics /></div>
+      <div id="about" className="pt-20"><About /></div>
+      <div id="powered" className="pt-20"><Powered /></div>
+      <div id="sabi" className="pt-20"><Sabi /></div>
+      <Pricing />
+      <div id="faqs" className="pt-20"><FAQs /></div>
+
+      <div id="footer" className="pt-20"><Footer /></div>
     </>
   )
 }
